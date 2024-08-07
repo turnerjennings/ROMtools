399daa75e8b2d5b2d5e2a9b21cf0527eacf50901
turnerjennings <turner.jennings@outlook.com>
2024-08-07 08:35:31 -0400

    update filetree

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ROMtools
 Functions for reduced order rigid body modelling of dynamical systems
+
+## Overview
+
+Functions for building rigid body dynamics simulations of more complicated systems.  
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='ROMtools',
+    version='0.1.1dev0',
+    description='Tools for reduced order modelling of dynamical systems',
+    author='Turner Jennings',
+    author_email='turner.jennings@outlook.com',
+    url='https://github.com/turnerjennings/ROMtools',
+    license='MIT',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "numpy"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering"
+    ]
+)
